ccbea17f73bf9b698d384c5f517d2338f207655f
Cameron Kadre <cameronkadre@u.boisestate.edu>
2024-01-11 14:14:15 -0700

    1.11

---
+++ MagicSquare/MagicSquare.java
@@ -0,0 +1,16 @@
+public class MagicSquare implements MagicSquareInterface
+{
+
+    @Override
+    public boolean isMagicSquare() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMagicSquare'");
+    }
+
+    @Override
+    public int[][] getMatrix() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMatrix'");
+    }
+    
+}
