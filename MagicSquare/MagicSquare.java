f32abe27724b5e63319db841060abe2c91d02de9
Cameron Kadre <cameronkadre@u.boisestate.edu>
2024-01-11 14:39:16 -0700

    1.11

--- MagicSquare/MagicSquare.java
+++ MagicSquare/MagicSquare.java
@@ -1,16 +1,75 @@
+//imports
+/**
+ * Description
+ * 
+ * @author Cameron Kadre
+ * @version Spring 2024
+ */
+
 public class MagicSquare implements MagicSquareInterface
 {
+    //instance vars
+    private boolean isMagicSquare;
+    private int[][] matrix;
 
+    //constructors
+
+    private MagicSquare(String fileName)
+    {
+
+    }
+
+    private MagicSquare(String fileName, int n)
+    {
+        
+    }
+    //methods
+	/**
+	 * Evaluate the matrix (whether read from file or
+	 * generated) and return a boolean verdict of
+	 * whether the matrix is a magic square.
+	 * 
+	 * @return true if matrix is a magic square, else false
+	 */
     @Override
-    public boolean isMagicSquare() {
+    public boolean isMagicSquare() 
+    {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isMagicSquare'");
     }
 
+	/**
+	 * Return a copy of the matrix (whether read from file 
+	 * or generated) as a 2D array of ints. Be sure this
+	 * method does not compromise encapsulation.
+	 * 
+	 * @return 2D array of ints that may or may not be a valid magic square
+	 */
     @Override
-    public int[][] getMatrix() {
+    public int[][] getMatrix() 
+    {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getMatrix'");
     }
     
+	/**
+	 * Return a string formatted as in these examples:
+	 *   The matrix
+	 *     4 9 2
+	 *     3 5 7
+	 *     8 1 6
+	 *   is a magic square.
+	 *  or
+	 *    The matrix
+	 *      5 3 7
+	 *      9 4 2
+	 *      1 6 5
+	 *    is not a magic square.
+	 *    
+	 * @return formatted string showing the matrix and whether it is a valid magic square
+	 */
+	public String toString()
+    {
+
+    }
 }
--- MagicSquare/MagicSquareDriver.java
+++ MagicSquare/MagicSquareDriver.java
@@ -1,4 +1,17 @@
+//imports
+/**
+ * Description
+ * 
+ * @author Cameron Kadre
+ * @version Spring 2024
+ */
+
 public class MagicSquareDriver
 {
+    public static void main(String[] args)
+    {
+        String operation = args[0];
+        System.out.print(operation);
+    }
 
 }
